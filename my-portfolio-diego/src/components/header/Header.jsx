636a7e3ab07e6e775f8bf8235b3efbc49bfc40e7
DrCloudFactory454 <diru@cloudfactory.dk>
2022-07-11 21:17:43 +0200

    CV implemented

--- my-portfolio-diego/src/components/header/Header.jsx
+++ my-portfolio-diego/src/components/header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
+import ME from '../../assets/Me.jpeg'
 
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Diego Ruiz</h1>
+        <h5 className="text light">FullStack Developer</h5>
+        <CTA />
+
+        <div className="Me">
+          <img src={ME} alt="me"/>
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+
+      </div>
+    </header>
     
   )
 }
