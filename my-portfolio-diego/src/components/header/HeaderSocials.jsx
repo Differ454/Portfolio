cd1b5baf1f2aa0e2ad71692315745d5bc1c8f2bb
DrCloudFactory454 <diru@cloudfactory.dk>
2022-07-11 21:39:39 +0200

    FaDribbbleSquare added

--- my-portfolio-diego/src/components/header/HeaderSocials.jsx
+++ my-portfolio-diego/src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
-import {AiFillDribbbleSquare} from 'react-icons/fa'
+import {FaDribbbleSquare} from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>
         <a href='https://github.com' target="_blank"><FaGithub/></a>
-        <a href='https://dribbble.com' target="_blank"><AiFillDribbbleSquare/></a>
+        <a href='https://dribbble.com' target="_blank"><FaDribbbleSquare/></a>
 
     </div>
   )
