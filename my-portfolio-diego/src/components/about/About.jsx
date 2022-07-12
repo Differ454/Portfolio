04da7bb0f512b50304baf00795089b172fe768ad
DrCloudFactory454 <diru@cloudfactory.dk>
2022-07-12 15:03:42 +0200

    dr picture implemented

--- my-portfolio-diego/src/components/about/About.jsx
+++ my-portfolio-diego/src/components/about/About.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
+import ME from '../../assets/dr.png'
 
 const About = () => {
   return (
-    <section id='about'>
-     About
-    </section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src="" alt="" />
+          </div>
+        </div>
+
+        <div className="about__content"></div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
