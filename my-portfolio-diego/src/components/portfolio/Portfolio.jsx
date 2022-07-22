da3422b23737b0e38e45dd47e2bfaa1faa701f42
DrCloudFactory454 <diru@cloudfactory.dk>
2022-07-22 23:01:42 +0200

    changes implemented

--- my-portfolio-diego/src/components/portfolio/Portfolio.jsx
+++ my-portfolio-diego/src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,59 @@
 import React from "react";
 import "./Portfolio.css";
 import STW from "../../assets/software.jpg";
+import RATE from "../../assets/rate.png";
+import NJN from "../../assets/ninja.jpg";
 // import STW from "../../assets/software.jpg";
 // import STW from "../../assets/software.jpg";
 // import STW from "../../assets/software.jpg";
-// import STW from "../../assets/software.jpg";
-// import STW from "../../assets/software.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: RATE,
+    title: "Rate-My-Cv-App",
+    github: "https://github.com/DrCloudFactory454/Rate-My-Cv-App",
+    demo: "https://rate-my-cv-production.herokuapp.com/",
+  },
+
+  {
+    id: 2,
+    image: NJN,
+    title: "Boomerang Ninja Game",
+    github: "https://github.com/DrCloudFactory454/Boomerang-Ninja-Game",
+    demo: "https://js13kgames.com/games/bounce-back/index.html",
+  },
+
+  {
+    id: 3,
+    image: STW,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/18861525-Lendary-Mobile",
+  },
+
+  {
+    id: 4,
+    image: STW,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/18861525-Lendary-Mobile",
+  },
+  {
+    id: 5,
+    image: STW,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/18861525-Lendary-Mobile",
+  },
+  {
+    id: 6,
+    image: STW,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/18861525-Lendary-Mobile",
+  },
+];
 
 const Portfolio = () => {
   return (
@@ -14,109 +62,24 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-      <article className="portfolio__item">
-        <div className="portfolio item-image">
-          <img className="ptc" src={STW} alt="" />
-        </div>
-        <h3>This is a portfolio item title</h3>
-        <a href="http://github.com" className="btn">
-          Github
-        </a>
-        <a
-          href="https://dribbble.com/shots/18859381-DJ-Dog-Basket-NFT"
-          className="btn btn-primary"
-          target="_blank"
-        >
-          Live Demo
-        </a>
-      </article>
-      
-
-      <article className="portfolio__item">
-        <div className="portfolio item-image">
-          <img className="ptc" src={STW} alt="" />
-        </div>
-        <h3>This is a portfolio item title</h3>
-        <a href="http://github.com" className="btn">
-          Github
-        </a>
-        <a
-          href="https://dribbble.com/shots/18859381-DJ-Dog-Basket-NFT"
-          className="btn btn-primary"
-          target="_blank"
-        >
-          Live Demo
-        </a>
-      </article>
-      
-
-      <article className="portfolio__item">
-        <div className="portfolio item-image">
-          <img className="ptc" src={STW} alt="" />
-        </div>
-        <h3>This is a portfolio item title</h3>
-        <a href="http://github.com" className="btn">
-          Github
-        </a>
-        <a
-          href="https://dribbble.com/shots/18859381-DJ-Dog-Basket-NFT"
-          className="btn btn-primary"
-          target="_blank"
-        >
-          Live Demo
-        </a>
-      </article>
-
-      <article className="portfolio__item">
-        <div className="portfolio item-image">
-          <img className="ptc" src={STW} alt="" />
-        </div>
-        <h3>This is a portfolio item title</h3>
-        <a href="http://github.com" className="btn">
-          Github
-        </a>
-        <a
-          href="https://dribbble.com/shots/18859381-DJ-Dog-Basket-NFT"
-          className="btn btn-primary"
-          target="_blank"
-        >
-          Live Demo
-        </a>
-      </article>
-
-      <article className="portfolio__item">
-        <div className="portfolio item-image">
-          <img className="ptc" src={STW} alt="" />
-        </div>
-        <h3>This is a portfolio item title</h3>
-        <a href="http://github.com" className="btn">
-          Github
-        </a>
-        <a
-          href="https://dribbble.com/shots/18859381-DJ-Dog-Basket-NFT"
-          className="btn btn-primary"
-          target="_blank"
-        >
-          Live Demo
-        </a>
-      </article>
-
-      <article className="portfolio__item">
-        <div className="portfolio item-image">
-          <img className="ptc" src={STW} alt="" />
-        </div>
-        <h3>This is a portfolio item title</h3>
-        <a href="http://github.com" className="btn">
-          Github
-        </a>
-        <a
-          href="https://dribbble.com/shots/18859381-DJ-Dog-Basket-NFT"
-          className="btn btn-primary"
-          target="_blank"
-        >
-          Live Demo
-        </a>
-      </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio item-image">
+                <img className="ptc" src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
